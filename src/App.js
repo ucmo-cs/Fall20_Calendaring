d15d3bd0e8ac0b815f3d523e41fbb848303cbdf1
Joshua <jxd78220@ucmo.edu>
2020-10-26 13:39:58 -0500

    Set up react-router-dom to allow for linking to multiple pages through a navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navigation, Home, NewAccount, Login } from "./components";
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/new-account" exact component={() => <NewAccount />} />
+          <Route path="/login" exact component={() => <Login />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
