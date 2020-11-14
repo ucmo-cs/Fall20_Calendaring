791810d2620513c3136de62c10ce08a181f19d4f
chukUDOT <72043873+chukUDOT@users.noreply.github.com>
2020-11-14 13:24:02 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+  
+export { default as Navigation } from "./Navigation";
+export { default as Home } from "./Home";
+export { default as NewAccount } from "./NewAccount";
+export { default as Login } from "./Login";
