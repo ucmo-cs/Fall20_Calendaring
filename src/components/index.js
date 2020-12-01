8423352adfcf63f2690be2618070448d213007c0
Joshua <jxd78220@ucmo.edu>
2020-11-30 22:09:29 -0600

    Test deleting create-react-app files

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-  
-export { default as Navigation } from "./Navigation";
-export { default as Home } from "./Home";
-export { default as NewAccount } from "./NewAccount";
-export { default as Login } from "./Login";
