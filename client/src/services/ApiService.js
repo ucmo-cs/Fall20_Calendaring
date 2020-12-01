c0e5ba49fb7f57e4f10bd8b5d727556220246c17
KingPeter1024 <47261552+KingPeter1024@users.noreply.github.com>
2020-12-01 15:51:02 -0600

    Update ApiService.js

--- client/src/services/ApiService.js
+++ client/src/services/ApiService.js
@@ -1,28 +1,32 @@
 import axios from 'axios';
 
-const CAR_API_BASE_URL = 'http://localhost:8080/cars';
+const API_BASE_URL = 'http://localhost:8080/api';
 
 class ApiService {
 
-    fetchCars() {
-        return axios.get(CAR_API_BASE_URL);
+    fetchAccounts(usr, pwd) {
+        //By username and password
+        return axios.get(API_BASE_URL + '/accounts/' + usr + '/' + pwd);
     }
 
-    fetchCarById(carId) {
-        return axios.get(CAR_API_BASE_URL + '/' + carId);
+    fetchTasks(account_id) {
+        return axios.get(API_BASE_URL + '/tasks/' + account_id);
+    }
+    addTask(account_id, calendar_id, title, description) {
+        return axios.post(API_BASE_URL, + 'tasks' + '/' + account_id + '/' + calendar_id + '/' + title + '/' + description);
     }
 
-    deleteCar(carId) {
-        return axios.delete(CAR_API_BASE_URL + '/' + carId);
-    }
+    // deleteCar(carId) {
+    //     return axios.delete(CAR_API_BASE_URL + '/' + carId);
+    // }
 
-    addCar(car) {
-        return axios.post(""+CAR_API_BASE_URL, car);
-    }
+    // addCar(car) {
+    //     return axios.post(""+CAR_API_BASE_URL, car);
+    // }
 
-    editCar(car) {
-        return axios.put(CAR_API_BASE_URL + '/' + car.id, car);
-    }
+    // editCar(car) {
+    //     return axios.put(CAR_API_BASE_URL + '/' + car.id, car);
+    // }
 
 }
 
