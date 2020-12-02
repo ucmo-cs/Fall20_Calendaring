43f4eea165971c2f431092a5855b2ccc84768352
KingPeter1024 <47261552+KingPeter1024@users.noreply.github.com>
2020-12-01 20:05:16 -0600

    Update app.js

--- client/src/app.js
+++ client/src/app.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Navigation, Home, NewAccount, Login } from "./components";
+import { Navigation, Home, NewAccount, Login, ShareCalendar } from "./components";
 import 'bootstrap/dist/css/bootstrap.min.css';
 const ReactDOM = require('react-dom');
 
 function App() {
+	const [account_id, setAccountID] = useState(null)
+
 	return (
+		account_id === null ?
 		<div className="container">
 			<Router>
 				<Navigation />
@@ -14,6 +17,19 @@ function App() {
 						<Route path="/" exact component={Home} />
 						<Route path="/new-account" component={NewAccount} />
 						<Route path="/login" component={Login} />
+					</Switch>
+			</Router>
+		</div>
+		:
+		<div className="container">
+			<Router>
+				<Navigation />
+					<h1 className="text-center" style={style}>Calendar App</h1>
+					<Switch>
+						<Route path="/" exact component={Home} />
+						<Route path="/new-account" component={NewAccount} />
+						<Route path="/login" component={Login} />
+						<Route path="/share-calendar" component={ShareCalendar} />
 					</Switch>
 			</Router>
 		</div>
