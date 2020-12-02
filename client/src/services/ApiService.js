62c00eb3900fbd178b570ecc28553e48ce549ae6
KingPeter1024 <47261552+KingPeter1024@users.noreply.github.com>
2020-12-01 19:15:42 -0600

    Update ApiService.js

--- client/src/services/ApiService.js
+++ client/src/services/ApiService.js
@@ -1,21 +1,33 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'http://localhost:8080/';
 
 class ApiService {
 
+    newAccount(username, email, password) {
+        return axios.post(API_BASE_URL, + 'accounts' + '/' + username + '/' + email + '/' + password);
+    }
+    fetchAccountIds() {
+        return axios.get(API_BASE_URL + 'accounts')
+    }
     fetchAccounts(usr, pwd) {
         //By username and password
-        return axios.get(API_BASE_URL + '/accounts/' + usr + '/' + pwd);
+        return axios.get(API_BASE_URL + 'accounts/' + usr + '/' + pwd);
     }
 
+    fetchCalendars(account_id) {
+        return axios.get(API_BASE_URL + 'calendars/' + account_id);
+    }
     fetchTasks(account_id) {
-        return axios.get(API_BASE_URL + '/tasks/' + account_id);
+        return axios.get(API_BASE_URL + 'tasks/' + account_id);
     }
     addTask(account_id, calendar_id, title, description) {
         return axios.post(API_BASE_URL, + 'tasks' + '/' + account_id + '/' + calendar_id + '/' + title + '/' + description);
     }
 
+    shareCalendar(calendar_id, account_id) {
+        return axios.post(""+API_BASE_URL, + 'calendars', account_id, calendar_id);
+    }
     // deleteCar(carId) {
     //     return axios.delete(CAR_API_BASE_URL + '/' + carId);
     // }
